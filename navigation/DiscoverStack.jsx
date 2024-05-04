ee73d53a208bebc43ef50fa14194045bfbfd9894
Sujith Varughese <sujithv15@gmail.com>
2024-05-04 05:28:44 -0400

    updates

--- navigation/DiscoverStack.jsx
+++ navigation/DiscoverStack.jsx
@@ -2,12 +2,14 @@ import { View, StyleSheet, useWindowDimensions } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LibraryScreen from '../screens/LibraryScreen'
 import DiscoverScreen from '../screens/DiscoverScreen'
+import BookScreen from '../screens/BookScreen'
 
 const Stack = createNativeStackNavigator()
 const DiscoverStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Discover">
       <Stack.Screen name="Discover" component={DiscoverScreen} />
+      <Stack.Screen name="Book" component={BookScreen} />
     </Stack.Navigator>
   )
 }
---
+++ screens/BookScreen.jsx
@@ -0,0 +1,14 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+
+const BookScreen = ({ title, author, coverImage, description, buyLinks }) => {
+  return (
+    <ScrollView>
+
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+export default BookScreen
