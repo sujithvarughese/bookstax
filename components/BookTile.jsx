8d0ffcd52dd1c6393159767608cea70f67449273
Sujith Varughese <sujithv15@gmail.com>
2024-05-05 16:11:22 -0400

    book details modal

---
+++ components/BookTile.jsx
@@ -0,0 +1,41 @@
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
+import BookDetails from '../BookDetails'
+import { useState } from 'react'
+const BookTile = ({ book }) => {
+
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.touchable} onPress={() => setShowModal(!showModal)}>
+          <Image resizeMode="cover" style={styles.image} source={{ uri: book.image }} />
+      </TouchableOpacity>
+      {showModal && <BookDetails showModal={showModal}/>}
+
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    marginRight: 12,
+    backgroundColor: "white",
+    elevation: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    borderRadius: 10,
+  },
+  image: {
+    borderRadius: 4,
+    width: 85,
+    height: 140,
+  },
+  touchable: {
+
+  },
+})
+export default BookTile
